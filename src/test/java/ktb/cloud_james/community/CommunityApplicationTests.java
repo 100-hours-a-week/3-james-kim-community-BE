db106ff240cfb2ec5ace7694497e283b15f1c2fc
Jongmin Kim <jmkim990226@gmail.com>
2025-10-03 16:38:03 +0900

    chore: 초기 프로젝트 설정

---
+++ src/test/java/ktb/cloud_james/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package ktb.cloud_james.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
